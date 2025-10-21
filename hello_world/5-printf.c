5b54a72249f2a03063acd4e5927e390e2b65286c
Denyss <paricarddenyss@gmail.com>
2025-10-21 14:10:44 +0000

    push

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche un message avec printf
+ *
+ * Description: Cette fonction affiche le message
+ * "with proper grammar, but the outcome is a piece of art,"
+ * suivi d’un retour à la ligne à l'aide de la fonction printf.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
