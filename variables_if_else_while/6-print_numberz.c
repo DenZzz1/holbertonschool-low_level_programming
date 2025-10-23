a21f342e74425ea48b24ed32a4e419f1791c8672
Denyss <paricarddenyss@gmail.com>
2025-10-23 13:38:28 +0000

    push

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche tous les nombres à un chiffre de base 10 à partir de 0.
+ *
+ * Description: Ce programme affiche les chiffres de 0 à 9 en utilisant uniquement
+ * la fonction putchar et sans utiliser aucune variable de type char.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n + '0');
+	putchar('\n');
+
+	return (0);
+}
