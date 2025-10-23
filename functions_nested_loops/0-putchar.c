c7d8cc7b46e1c6d63d3b74cf14eee8cbc95c2be8
Denyss <paricarddenyss@gmail.com>
2025-10-23 14:29:27 +0000

    push

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line
+ *
+ * Description: This program uses the _putchar function
+ * to print the string "_putchar" on one line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
