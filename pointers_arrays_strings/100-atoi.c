e32e6f6796a751c5634d6ee7dffbc81e4d3b80c6
DenZzz1 <paricarddenyss@gmail.com>
2025-10-31 20:30:09 +0000

    push

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -8,20 +9,24 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
+	int i = 0, sign = 1, started = 0;
 	int result = 0;
-	int started = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
-
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			int digit = s[i] - '0';
+
+			if (sign == 1 && (result > (INT_MAX - digit) / 10))
+				return (INT_MAX);
+			if (sign == -1 && (-result < (INT_MIN + digit) / 10))
+				return (INT_MIN);
+
+			result = (result * 10) + digit;
 			started = 1;
-			result = (result * 10) + (s[i] - '0');
 		}
 		else if (started)
 			break;
