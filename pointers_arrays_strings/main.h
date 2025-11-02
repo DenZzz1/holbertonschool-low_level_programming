07b86d170fefd0222f08a950f3f2dc1d3b944b9e
DenZzz1 <paricarddenyss@gmail.com>
2025-11-02 14:40:48 +0000

    push

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,9 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
+int _strlen(char *s);
+int _atoi(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -12,8 +13,8 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
