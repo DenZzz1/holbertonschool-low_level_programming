bfdd7ac31f0b928b15c216a50ab5cc71f25dfa42
DenZzz1 <paricarddenyss@gmail.com>
2025-11-03 10:04:26 +0000

    push

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,5 +21,6 @@ char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
