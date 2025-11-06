514dfd6159908fac65e750322896dbcc6ff4e8ce
Denyss <paricarddenyss@gmail.com>
2025-11-06 00:37:13 +0000

    push

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,12 +4,12 @@
  * _strlen_recursion - returns the length of a string
  * @s: pointer to the string
  *
- * Return: the length of the string
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
 		return (0);
-
-	return (1 + _strlen_recursion(s + 1));
 }
