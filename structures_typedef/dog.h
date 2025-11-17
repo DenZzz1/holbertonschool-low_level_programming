e7bcb2d62c4f3361a531e68d7e032698e30aa1bf
Denyss <paricarddenyss@gmail.com>
2025-11-17 11:41:05 +0000

    push

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,22 +2,18 @@
 #define DOG_H
 
 /**
- * struct dog - structure describing a dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * struct dog - structure representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
-struct dog
+typedef struct dog
 {
-char *name;
-float age;
-char *owner;
-}
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
