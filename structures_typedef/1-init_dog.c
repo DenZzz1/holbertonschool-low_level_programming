5843b9958b51cdfa9b6584876504510ddefa60d3
Denyss <paricarddenyss@gmail.com>
2025-11-17 11:45:33 +0000

    push

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,19 +1,18 @@
-#include <stddef.h>
 #include "dog.h"
 
 /**
- * init_dog - initializes a variable of type struct dog
- * @d: pointer to struct dog to initialize
- * @name: name to assign
- * @age: age to assign
- * @owner: owner to assign
+ * init_dog - initializes a dog struct
+ * @d: pointer to struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(dog_t *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
---
+++ structures_typedef/main_0.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+
+int main(void)
+{
+	dog_t my_dog;
+
+	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+
+	printf("Name: %s\n", my_dog.name);
+	printf("Age: %.2f\n", my_dog.age);
+	printf("Owner: %s\n", my_dog.owner);
+
+	return (0);
+}
