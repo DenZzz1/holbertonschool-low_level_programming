c570893b6defaef40b34adf1675ac0fd71c217ac
Denyss <paricarddenyss@gmail.com>
2025-11-19 15:33:13 +0000

    push

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,22 +11,22 @@
  * Description: If separator is NULL, it is not printed between numbers.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
+
 {
-    va_list args;
-    unsigned int i;
+va_list args;
+unsigned int i;
 
-    va_start(args, n);
+va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        int num = va_arg(args, int);
-        printf("%d", num);
+for (i = 0; i < n; i++)
+{
+int num = va_arg(args, int);
+printf("%d", num);
 
-        if (separator != NULL && i < n - 1)
-            printf("%s", separator);
-    }
+if (separator != NULL && i < n - 1)
+printf("%s", separator);
+}
+va_end(args);
 
-    va_end(args);
-
-    printf("\n");
+printf("\n");
 }
