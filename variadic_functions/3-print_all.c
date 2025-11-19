e25335e7a5aa75906ba21b1e40d2ba3ce7eb314d
Denyss <paricarddenyss@gmail.com>
2025-11-19 15:52:09 +0000

    push

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,47 +7,32 @@
  * @format: String where each char is a type: 'c', 'i', 'f', 's'.
  */
 void print_all(const char * const format, ...)
-
 {
-
 va_list args;
 unsigned int i = 0;
 char *s;
 char sep[] = ", ";
 const char *psep = "";
-
 va_start(args, format);
-
 while (format && format[i])
-
 {
 if (format[i] == 'c')
 printf("%s%c", psep, va_arg(args, int)), psep = sep;
-
 if (format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-
 {
-
 if (format[i] == 'i')
 printf("%s%d", psep, va_arg(args, int));
-
 if (format[i] == 'f')
 printf("%s%f", psep, va_arg(args, double));
-
 if (format[i] == 's')
-
 {
 s = va_arg(args, char *);
 printf("%s%s", psep, s ? s : "(nil)");
-
 }
 psep = sep;
-
 }
 i++;
-
 }
-
 va_end(args);
 printf("\n");
 }
