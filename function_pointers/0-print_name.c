2e49fc94945e758f4f36a4c9c58b9b74087a965b
Denyss <paricarddenyss@gmail.com>
2025-11-19 14:04:02 +0000

    push

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,16 +1,19 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using a function pointer
- * @name: the name to print
- * @f: function pointer to the printing function
+ * print_name - prints a name using a callback function
+ * @name: pointer to the name to print
+ * @f: pointer to the function to use for printing
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name == NULL || f == NULL)
-        return;
+	if (name == NULL)
+		return;
 
-    f(name);
+	if (f == NULL)
+		return;
+
+	f(name);
 }
