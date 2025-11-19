722a6f6283e9f0c348cb7ad729b8932ef28b79c2
Denyss <paricarddenyss@gmail.com>
2025-11-19 16:30:38 +0000

    push

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+
 /**
 * print_int - prints an int
 * @args: the list of args
@@ -9,10 +10,20 @@ void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
+
+/**
+* print_char - prints a char
+* @args: the list of args
+*/
 void print_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
+
+/**
+* print_string - prints a string
+* @args: the list of args
+*/
 void print_string(va_list args)
 {
 	char *z = va_arg(args, char *);
@@ -21,6 +32,11 @@ void print_string(va_list args)
 	else
 		printf("(nil)");
 }
+
+/**
+* print_all - prints all
+* @format: formats of arg
+*/
 void print_all(const char * const format, ...)
 {
 	types_t types[] = {
@@ -29,13 +45,14 @@ void print_all(const char * const format, ...)
 	va_list args;
 	unsigned int i = 0, j;
 	const char *sep = "";
+
 	va_start(args, format);
-	while (format && format[i])
+	while (format && format[i])        /* 1er while */
 	{
 		j = 0;
-		while (types[j].t)
+		while (types[j].t)             /* 2e while */
 		{
-			if (*(types[j].t) == format[i])
+			if (*(types[j].t) == format[i])  /* 1er if */
 			{
 				printf("%s", sep);
 				types[j].f(args);
