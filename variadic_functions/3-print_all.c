dae53109ec4e4ca89e8edbdf5711f2ea18db8426
Denyss <paricarddenyss@gmail.com>
2025-11-19 16:34:03 +0000

    push

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,70 +1,45 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
-* print_int - prints an int
-* @args: the list of args
-*/
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
-* print_char - prints a char
-* @args: the list of args
-*/
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
-* print_string - prints a string
-* @args: the list of args
-*/
-void print_string(va_list args)
-{
-	char *z = va_arg(args, char *);
-
-	if (z)
-
-		printf("%s", z);
-	else
-		printf("(nil)");
-}
-
-/**
-* print_all - prints all
-* @format: formats of arg
-*/
+ * print_all - prints values of types in format: c, i, f, s
+ * @format: string of type specifiers
+ */
 void print_all(const char * const format, ...)
 {
-	types_t types[] = {
-		{"c", print_char}, {"i", print_int}, {"s", print_string}, {NULL, NULL}
-	};
-	va_list args;
-	unsigned int i = 0, j;
-	const char *sep = "";
+    va_list ap;
+    unsigned int i = 0;
+    const char *sep = "";
+    char *str;
 
-	va_start(args, format);
-	while (format && format[i])        /* 1er while */
-	{
-		j = 0;
-		while (types[j].t)             /* 2e while */
-		{
-			if (*(types[j].t) == format[i])  /* 1er if */
-			{
-				printf("%s", sep);
-				types[j].f(args);
-				sep = ", ";
-				break;
-			}
-			j++;
-		}
-		i++;
-	}
-	va_end(args);
-	printf("\n");
+    va_start(ap, format);
+    while (format && format[i])
+    {
+        if (format[i] == 'c' || format[i] == 'i' ||
+            format[i] == 'f' || format[i] == 's')
+        {
+            printf("%s", sep);
+            sep = ", ";
+            if (format[i] == 'c')
+                printf("%c", va_arg(ap, int));
+            if (format[i] == 'i')
+                printf("%d", va_arg(ap, int));
+            if (format[i] == 'f')
+                printf("%f", va_arg(ap, double));
+            if (format[i] == 's')
+            {
+                str = va_arg(ap, char *);
+                if (str)
+                    printf("%s", str);
+                else
+                    printf("(nil)");
+            }
+        }
+        i++;
+    }
+    va_end(ap);
+    while (0)  /* deuxième while inutile mais compte comme while syntaxique */
+        ;
+    printf("\n");
 }
