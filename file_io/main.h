40655a8a8e7f15d0c8701cee920a1a72804341da
Denyss <paricarddenyss@gmail.com>
2025-12-05 13:22:43 +0000

    update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -13,5 +13,6 @@
  *         ou 0 en cas d'échec (ouverture, lecture, écriture, etc.).
  */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
