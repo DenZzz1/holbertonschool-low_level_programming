71a4103f7a60b62371ffbb93fa013acbefe7afd5
Denyss <paricarddenyss@gmail.com>
2025-12-05 04:41:49 +0000

    add 1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the head of the list
+ *
+ * Description: This function counts all the nodes in a singly linked list
+ * by traversing it from head to NULL.
+ *
+ * Return: the number of elements (nodes) in the list
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+
+return (count);
+}
