c8f38c08ef1ab5bf7aa57b1ed3e33e5c6a732139
Denyss <paricarddenyss@gmail.com>
2025-12-05 06:29:58 +0000

    add main.h

---
+++ file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <sys/types.h>
+
+/**
+ * read_textfile - lit un fichier texte et l'affiche sur la sortie standard.
+ * @filename: le nom du fichier à lire.
+ * @letters: le nombre maximum de lettres (octets) à lire et afficher.
+ *
+ * Return: le nombre réel de lettres lues et écrites,
+ *         ou 0 en cas d'échec (ouverture, lecture, écriture, etc.).
+ */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
